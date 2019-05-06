001b0064a93040126c4b6ba36ac8ec29d525e026
Lorenzo <lgdandrea4@gmail.com>
2019-05-06 18:44:49 -0500

    was able to get my application functional and render gif images.  Unfortunately after I render the images I run into an issue where my functions can no longer fire again to recreate actions on dynamic functions.  Still working on that resolution

---
+++ assets/javascript/api.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+    
+    var topics = ["Lion King", "Beauty and the Beast", "Moana", "Sleeping Beauty", "Snow White", "Cinderella"];
+    
+    
+    
+    function renderButtons () {
+        $("#buttons").empty();
+        for (let i = 0; i<topics.length; i++) {
+            let btn = $("<button>");
+            btn.addClass("movie");
+            btn.attr("title", topics[i]);
+            btn.text(topics[i]);
+            $("#buttons").append(btn);           
+        }
+    }
+    
+    $("#submit").on("click", function(event){
+        event.preventDefault();
+        var topic = $("#gif-search").val().trim();
+        topics.push(topic);
+        renderButtons();
+    });
+    
+    
+    renderButtons();
+    
+    $(".movie").on("click", function(){
+        var title  = $(this).attr("title");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + title + "&api_key=eSl0jNIgxwpPAairAjjs9x5pCXyGulyO&limit=10"
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function(response) {
+            console.log(response);
+            for (let i = 0; i<response.data.length; i++){
+                var imageURL = response.data[i].images.downsized_still.url;
+                var imageAnimate = response.data[i].images.downsized_medium.url;
+                var image = $("<img>");
+                image.attr("src", imageURL);
+                image.addClass("gif");
+                image.attr("data-state", "still");
+                image.attr("data-still", imageURL);
+                image.attr("data-animate", imageAnimate);
+                image.attr("alt", "title");
+                $("#gifs-view").prepend(image);
+            }
+        });
+
+        console.log(title)
+    });
+    
+    $(".gif").on("click", function(){
+        var state = $(this).attr("data-state");
+            
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "animate");
+        }
+    });
+
+    function alertMe() {
+        console.log("hello!", $(this).attr("title"));
+    }
+
+    $(document).on("click", ".movie" );
+    $(document).on("click", ".gif");
+    
+});
