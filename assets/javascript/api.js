70052d42b05de54e66d18126c405250136a48b25
Lorenzo <lgdandrea4@gmail.com>
2019-05-06 19:17:28 -0500

    a fully functional website with all the trimings... working on some CSS to see if I can get it looking nice

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -3,29 +3,7 @@ $(document).ready(function () {
     var topics = ["Lion King", "Beauty and the Beast", "Moana", "Sleeping Beauty", "Snow White", "Cinderella"];
     
     
-    
-    function renderButtons () {
-        $("#buttons").empty();
-        for (let i = 0; i<topics.length; i++) {
-            let btn = $("<button>");
-            btn.addClass("movie");
-            btn.attr("title", topics[i]);
-            btn.text(topics[i]);
-            $("#buttons").append(btn);           
-        }
-    }
-    
-    $("#submit").on("click", function(event){
-        event.preventDefault();
-        var topic = $("#gif-search").val().trim();
-        topics.push(topic);
-        renderButtons();
-    });
-    
-    
-    renderButtons();
-    
-    $(".movie").on("click", function(){
+    $(document).on("click", ".movie", function(){
         var title  = $(this).attr("title");
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + title + "&api_key=eSl0jNIgxwpPAairAjjs9x5pCXyGulyO&limit=10"
         
@@ -47,28 +25,48 @@ $(document).ready(function () {
                 image.attr("alt", "title");
                 $("#gifs-view").prepend(image);
             }
-        });
+        });    
+    });
 
-        console.log(title)
+    $(document).on("click", ".gif", function(){
+            var state = $(this).attr("data-state");
+                
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
     });
     
-    $(".gif").on("click", function(){
-        var state = $(this).attr("data-state");
-            
-        if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-        } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "animate");
+    function renderButtons () {
+        $("#buttons").empty();
+        for (let i = 0; i<topics.length; i++) {
+            let btn = $("<button>");
+            btn.addClass("movie btn-info");
+            btn.attr("title", topics[i]);
+            btn.text(topics[i]);
+            $("#buttons").append(btn);           
         }
+    }
+    
+    $("#submit").on("click", function(event){
+        event.preventDefault();
+        var topic = $("#gif-search").val().trim();
+        topics.push(topic);
+        renderButtons();
     });
+    
+    
+    renderButtons();
+    
+
+    
 
     function alertMe() {
         console.log("hello!", $(this).attr("title"));
     }
 
-    // $(document).on("click", ".movie" );
-    // $(document).on("click", ".gif");
     
 });
